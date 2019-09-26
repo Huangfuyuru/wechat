a0d126641c23bb99390d3e4a0abbe2f28dd50979
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-09-26 08:38:48 +0800

    a

--- lesson-3/server-api-src/access_token.js
+++ lesson-3/server-api-src/access_token.js
@@ -1,11 +1,11 @@
-const wxkey = require('./weixinlib/wxkey');
 const gohttp = require('gohttp');
+const wxkey = require('./weixinlib/wxkey');
 
 var tokenurl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wxkey.appid}&secret=${wxkey.secret}`;
 
-
-gohttp.get(tokenurl).then(data => {
+gohttp.get(tokenurl).then((data,err)=>{
     console.log(data);
-}, err => {
-    console.log(err);
+    if(err){
+        console.log(err)
+    }    
 })
--- lesson-3/server-api-src/weixinlib/wxkey.js
+++ lesson-3/server-api-src/weixinlib/wxkey.js
@@ -1,4 +1,6 @@
 module.exports = {
-    appid : '',
-    secret : ''
+    appid : 'wxd20f65d0bbb6ab1b',
+    secret : 'f985e896b7749f4c799d6ad2db2c3f39'
 };
+
+
